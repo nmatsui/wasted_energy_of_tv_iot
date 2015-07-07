599c1d20f6430f3938aa8259d69cd41386c978df
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2015-07-07 15:19:20 +0000

    remove unnecessary print

--- lib/ConsulWrapper.py
+++ lib/ConsulWrapper.py
@@ -6,7 +6,6 @@ class ConsulWrapper(object):
     FACEDETECT_KEY = "face_detected"
 
     def __init__(self):
-        print "ConsulWrapper init"
         self.c = consul.Consul()
 
     def kv_put(self, key, value):
